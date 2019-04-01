7f406cac3d69d621765dd76ebb871d94028383c4
shubhamrules470 <shubhamrules470@gmail.com>
2019-04-01 12:06:46 +0530

    asdasdf

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template,request,url_for,redirect
 import sqlite3 as sq
+import sqlite3 as s
+con = s.connect('sarthak_ka_database.db')
+with con:
+    cur=con.cursor()
+    cur.execute("Create table if not exists details(fname text not null,lname text,email text primary key not null,password text not null,weight int not null,height int not null,profession text,gender text)")
 
 
 username=None
